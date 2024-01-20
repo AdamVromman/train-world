7fe1b6e5bcb486b303f1021227e828df1d8e724b
AdamVromman <adam.vromman@gmail.com>
2024-01-20 14:51:43 +0100

    Tailwind setup + page structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,52 @@
 export default {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        15: "15px",
+        30: "30px",
+        60: "60px",
+        120: "120px",
+        210: "210px",
+      },
+      margin: {
+        15: "15px",
+        30: "30px",
+        60: "60px",
+        120: "120px",
+        210: "210px",
+      },
+      gap: {
+        15: "15px",
+        30: "30px",
+        60: "60px",
+        120: "120px",
+        210: "210px",
+      },
+      screens: {
+        desktop: "1440px",
+        laptop: "1024px",
+        tablet: "768px",
+        phone: "400px",
+      },
+      colors: {
+        white: {
+          DEFAULT: "#E5DBC6",
+        },
+        black: {
+          DEFAULT: "#373737",
+        },
+        red: {
+          DEFAULT: "#CB352C",
+        },
+        yellow: {
+          DEFAULT: "#EEC32D",
+        },
+        teal: {
+          DEFAULT: "#B7D8CC",
+        },
+      },
+    },
   },
   plugins: [],
 };
