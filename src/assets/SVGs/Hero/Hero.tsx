e55b6b0368d94beb23662559ea28112eb42091f9
AdamVromman <adam.vromman@gmail.com>
2024-01-26 22:57:12 +0100

    Test header without train

--- src/assets/SVGs/Hero/Hero.tsx
+++ src/assets/SVGs/Hero/Hero.tsx
@@ -3,7 +3,6 @@ import { lazy } from "react";
 const Clouds = lazy(() => import("./Clouds"));
 const Mountains = lazy(() => import("./Mountains"));
 const Tracks = lazy(() => import("./Tracks"));
-const Train = lazy(() => import("./Train"));
 
 export const Hero = (
   <>
@@ -16,7 +15,7 @@ export const Hero = (
     {Clouds}
     {Mountains}
     {Tracks}
-    {Train}
+    {/* {Train} */}
     <g className="timeline--gradient-bars">
       <rect
         className="cls-6 stroke-width-0 fill-white"
